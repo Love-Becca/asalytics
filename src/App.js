a1bd2e79cebb02033e6a2054704d74b0a85d6f21
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-08-11 11:33:17 +0100

    add: apollo client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Header from './Components/Header';
 import Body from './Components/Body'
-
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+ 
+const client = new ApolloClient({
+  uri: 'https://analytics-api.herokuapp.com/analytics'
+})
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+    </ApolloProvider>
   );
 }
 
